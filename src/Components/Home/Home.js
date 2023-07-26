03ebd71c2bc8ee11340276990121fdfd0e0564d2
Prakhar Kumar Srivastava <prakhar25789@gmail.com>
2023-07-26 23:27:22 +0530

    ADD: Home Page in Masonry Layout

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import Header from '../Header/Header'
 import './Home.css'
+import Masonry, {ResponsiveMasonry} from 'react-responsive-masonry'
 
 function Home() {
   return (
     <div>
         <Header />
         <div className="imageFeeds">
-            <div className="imageFeed"><img src="/post1.jpeg" /></div>
-            <div className="imageFeed"><img src="/post2.jpeg" /></div>
-            <div className="imageFeed"><img src="/post3.jpg" /></div>
-            <div className="imageFeed"><img src="/post4.jpeg" /></div>
-            <div className="imageFeed"><img src="/post5.jpeg" /></div>
+            <ResponsiveMasonry columnsCountBreakPoints={{400: 2,600:3, 750: 4, 900: 6}}>
+            <Masonry columnsCount={1} gutter={"5px"}>
+            <img className='imageFeed' src="/post1.jpeg" />
+            <img className='imageFeed' src="/post2.jpeg" />
+            <img className='imageFeed' src="/post3.jpg" />
+            <img className='imageFeed' src="/post4.jpeg" />
+            <img className='imageFeed' src="/post5.jpeg" />
+            </Masonry>
+            </ResponsiveMasonry>
         </div>
     </div>
   )
