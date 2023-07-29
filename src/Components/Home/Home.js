2e360f4e2e9407b62897bde02cb75a3f821cd5fd
Prakhar Kumar Srivastava <prakhar25789@gmail.com>
2023-07-30 01:17:02 +0530

    FIX: Loader For Post Likes

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -8,9 +8,12 @@ import { useNavigate } from 'react-router-dom'
 function Home() {
 
     const [posts,setPosts]=useState([]);
+    const [postsLoader,setPostsLoader]=useState();
+    const [loading,setLoading]=useState(false);
     const navigate=useNavigate();
 
     useEffect(()=>{
+        setLoading(true);
         const url = process.env.REACT_APP_BACKEND_ENDPOINT+"posts/"
         fetch(url,{
             headers:{
@@ -19,18 +22,22 @@ function Home() {
         })
         .then(async(res)=>  await res.json())
         .then(({posts}) =>{
-            let tempPosts = posts.map((post)=> {
-                return {...posts,loading:false};
-            })
+            setLoading(false);
+            setPostsLoader(new Array(posts.length))
             setPosts(posts)
         })
-        .catch((e)=>console.log(e));
+        .catch((e)=>{
+            alert("Failed to Load the Posts, Try again after some time")
+        });
     },[])
 
-    const makeApiRequestToLike =async (post)=>{
+    const makeApiRequestToLike =async (post,index)=>{
         const id=post.id;
-        post.loading=true;
-        setPosts(posts.splice(posts.indexOf(post),1,post))
+        setPostsLoader((intialValue)=>{
+            let temp =[...intialValue];
+            temp[index]=true;
+            return temp;
+        })
         const url =process.env.REACT_APP_BACKEND_ENDPOINT+"post/like"
         await fetch(url,{
             method: 'POST',
@@ -40,19 +47,33 @@ function Home() {
               'postid':id,
             },  
             body: JSON.stringify({postId:id}),
-          }).then(()=>{
-                post.liked = !post.liked;
-                post.loading=false;
-                let tempPosts =posts.splice(posts.indexOf(post),1,post) 
+          }).then((res)=>{
+                if(res.ok) post.liked = !post.liked;
+                else if(res.status==403) {
+                    alert("Auth Token Expired, Please Login Again ")
+                    localStorage.removeItem("authorization");
+                    localStorage.removeItem("imageURL");
+                    localStorage.removeItem("userId");
+                    localStorage.removeItem("username");
+                    navigate('/user/login')
+                }
+                else alert(res.statusText);
+                let tempPosts=posts;
+                tempPosts[index]=post;
                 setPosts(tempPosts);
+                setPostsLoader((intialValue)=>{
+                    let temp =[...intialValue];
+                    temp[index]=false;
+                    return temp;
+                })
           })
         .catch((er)=> alert(er.message))
     }
 
-    const handleLikeClick =async (e,post)=>{
+    const handleLikeClick =async (e,post,index)=>{
         e.stopPropagation();    
         if(localStorage.getItem("authorization")) 
-        makeApiRequestToLike(post);
+        makeApiRequestToLike(post,index);
         else 
         alert("You must be logged in to like a post")
     }
@@ -62,6 +83,8 @@ function Home() {
         <Header />
         <div className="imageFeeds">
             {
+                loading ?
+                <div className="authBoxText">Loading Posts...</div>:
                 posts.length==0 &&
                 <h1 className="authBoxText">No Posts Available Currently</h1>
             }
@@ -72,9 +95,9 @@ function Home() {
                     return (
                     <div className='imageFeed' key={index} onClick={()=>navigate("/feed")}>
                         <img src={post.imageURL} />
-                        <button className="likeButton" onClick={(e)=> handleLikeClick(e,post)}>
+                        <button className="likeButton" onClick={(e)=> handleLikeClick(e,post,index)}>
                         {
-                            post.loading ? <div className="loader"></div> :
+                            postsLoader[index] && postsLoader[index]==true ? <div className="loader"></div> :
                             <img src={post.liked ? "/heartFilled.svg" : "heart.svg"} alt='likeButton'/>
                         } 
                         </button> 
